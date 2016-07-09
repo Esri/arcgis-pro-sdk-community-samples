9265bbf082656dfd22627a77279aacd47341c036
arcgisprosdk <arcgisprosdk@esri.com>
2016-07-08 19:21:24 -0700

    Added a new sample:  AddDeleteFieldToFromFeatureClass

---
+++ Geodatabase/AddDeleteFieldToFromFeatureClass/AddAField.cs
@@ -0,0 +1,95 @@
+/*
+
+   Copyright 2016 Esri
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ArcGIS.Core.Data;
+using ArcGIS.Desktop.Core.Geoprocessing;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Internal.Mapping;
+using ArcGIS.Desktop.Mapping;
+
+namespace AddDeleteFieldToFromFeatureClass
+{
+    internal class AddAField : Button
+    {
+        protected override async void OnClick()
+        {
+            try
+            {
+                BasicFeatureLayer layer = null;
+                await ArcGIS.Desktop.Framework.Threading.Tasks.QueuedTask.Run(() =>
+                {
+                    //find first layer
+                    layer = MapView.Active.Map.Layers.FirstOrDefault() as BasicFeatureLayer;
+
+                });
+                if (layer == null)
+                    MessageBox.Show("Unable to find a feature class at the first layer of the active map");
+                else
+                {
+                    MessageBox.Show($@"{layer.Name} was found .... adding a new Field");
+                    await
+                        ExecuteAddFieldTool(layer, new KeyValuePair<string, string>("AddedField", "Alias Name Added Field"), "Text", 50);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private async Task<bool> ExecuteAddFieldTool(BasicFeatureLayer theLayer, KeyValuePair<string, string> field, string fieldType, int? fieldLength = null, bool isNullable = true)
+        {
+            try
+            {
+                return await ArcGIS.Desktop.Framework.Threading.Tasks.QueuedTask.Run(() =>
+                {
+                    var inTable = theLayer.Name;
+                    var table = theLayer.GetTable();
+                    var dataStore = table.GetDatastore();
+                    var workspaceNameDef = dataStore.GetConnectionString();
+                    var workspaceName = workspaceNameDef.Split('=')[1];
+
+                    var parameters = Geoprocessing.MakeValueArray(inTable, field.Key, fieldType.ToUpper(), null, null,
+                        fieldLength, field.Value, isNullable ? "NULABLE" : "NON_NULLABLE");
+                    var env = Geoprocessing.MakeEnvironmentArray(workspace: workspaceName);
+                    var cts = new CancellationTokenSource();
+                    var results = Geoprocessing.ExecuteToolAsync("management.AddField", parameters, env, cts.Token,
+                        (eventName, o) =>
+                        {
+                            System.Diagnostics.Debug.WriteLine($@"GP event: {eventName}");
+                        });
+                    return true;
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Geodatabase/AddDeleteFieldToFromFeatureClass/Module1.cs
@@ -0,0 +1,78 @@
+/*
+
+   Copyright 2016 Esri
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using System.Threading.Tasks;
+
+namespace AddDeleteFieldToFromFeatureClass
+{
+    /// <summary>
+    /// This sample shows how to use Geo Processing to add and remove a fields from a geodatabase.
+    /// </summary>
+    /// <remarks>
+    /// 1. Download the Community Sample data (see under the "Resources" section for downloading sample data).  The sample data contains a project called "FeatureTest.aprx" with data suitable for this sample.  Make sure that the Sample data is unzipped in c:\data and "C:\Data\FeatureTest\FeatureTest.aprx" is available.
+    /// 1. Open this solution in Visual Studio 2015.  
+    /// 1. Click the build menu and select Build Solution.
+    /// 1. Click the Start button to open ArCGIS Pro. ArcGIS Pro will open.  
+    /// 1. Open the "C:\Data\FeatureTest\FeatureTest.aprx" project.  
+    /// 1. Click on the Add-in tab and verify that a "FeatureClass Schema" group was added.  
+    /// 1. Open the "Attribute table" for the topmost (first) layer.  This will later allow you to view the schema change in real time.
+    /// ![UI](Screenshots/Screen1.png)
+    /// 1. Note the field names in the attribute table for the first layer.
+    /// 1. Click the "Add a new field" button and check that the field "Alias Name Added Field" was added to the attribute table.
+    /// ![UI](Screenshots/Screen2.png
+    /// 1. Click the "Delete the new Field" button and check that the field "Alias Name Added Field" is successfully removed from the attribute table.
+    /// ![UI](Screenshots/Screen3.png)
+    /// </remarks>
+    internal class Module1 : Module
+    {
+        private static Module1 _this = null;
+
+        /// <summary>
+        /// Retrieve the singleton instance to this module here
+        /// </summary>
+        public static Module1 Current
+        {
+            get
+            {
+                return _this ?? (_this = (Module1)FrameworkApplication.FindModule("AddDeleteFieldToFromFeatureClass_Module"));
+            }
+        }
+
+        #region Overrides
+        /// <summary>
+        /// Called by Framework when ArcGIS Pro is closing
+        /// </summary>
+        /// <returns>False to prevent Pro from closing, otherwise True</returns>
+        protected override bool CanUnload()
+        {
+            //TODO - add your business logic
+            //return false to ~cancel~ Application close
+            return true;
+        }
+
+        #endregion Overrides
+
+    }
+}
